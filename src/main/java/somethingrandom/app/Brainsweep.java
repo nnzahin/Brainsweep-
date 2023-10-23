10f7f5bb2bd301fc157d27f2176001b6b4887363
Duncan McIntosh <duncan82013@live.ca>
2023-10-23 10:11:22 -0400

    general: add basic Java project structure
    This means we can use Maven dependencies, etc.

---
+++ src/main/java/somethingrandom/app/Brainsweep.java
@@ -0,0 +1,7 @@
+package somethingrandom.app;
+
+public class Brainsweep {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/PlaceholderTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaceholderTest {
+    @Test
+    public void addingIsCommutative() {
+        assertEquals(3, 2 + 1);
+        assertEquals(3, 1 + 2);
+    }
+}
