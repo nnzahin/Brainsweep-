b13f8a64f6809be870aa2df6aae91df96a780aea
Huda Sahaf <hudasahaf27@gmail.com>
2023-11-20 10:25:13 -0500

    finished adjusting the structure of AddItemView

--- src/main/java/somethingrandom/view/AddItemView.java
+++ src/main/java/somethingrandom/view/AddItemView.java
@@ -15,26 +15,33 @@ import java.beans.PropertyChangeListener;
 
 public class AddItemView extends JPanel implements ActionListener, PropertyChangeListener {
     public final String viewName = "add item";
-    private static JPanel panel = new JPanel();
-    private static JFrame frame = new JFrame();
-    private static JLabel itemTitle = new JLabel();
-    private static JTextField itemtitleInput = new JTextField(100);
-    private static JLabel itemDisc = new JLabel();
-    private static JTextField itemDiscInput = new JTextField(10);
-    private static JButton saveItem;
-    private final AddItemController itemController;
-    private AddItemViewModel addItemViewModel;
+    private final AddItemViewModel addItemViewModel;
+    private final JTextField itemTitleInput = new JTextField(100);
+    private final JTextField itemDiscInput = new JTextField(10);
+    private final AddItemController addItemController;
+    private final JButton saveItem;
 
+    //private static JPanel panel = new JPanel();
+    //private static JFrame frame = new JFrame();
+    //private static JLabel itemTitle = new JLabel();
+
+    //private static JLabel itemDisc = new JLabel();
 
     public AddItemView(AddItemController itemController, AddItemViewModel addItemViewModel) {
-        this.itemController = itemController;
+        this.addItemController = itemController;
         this.addItemViewModel = addItemViewModel;
+        addItemViewModel.addPropertyChangeListener(this);
 
         JLabel title = new JLabel("ADD ITEM");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        LabelTextPanel itemTitleInfo = new LabelTextPanel(
+            new JLabel(AddItemViewModel.TITLE_LABEL), itemTitleInput);
+        LabelTextPanel itemDiscInfo = new LabelTextPanel(
+            new JLabel(AddItemViewModel.DISC_LABEL), itemDiscInput);
+
         JPanel buttons = new JPanel();
-        saveItem = new JButton();
+        saveItem = new JButton(AddItemViewModel.SAVE_BUTTON_LABEL);
         buttons.add(saveItem);
 
         saveItem.addActionListener(
@@ -52,11 +59,11 @@ public class AddItemView extends JPanel implements ActionListener, PropertyChang
                 }
             }
         );
-        itemtitleInput.addKeyListener(new KeyListener() {
+        itemTitleInput.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
                 AddItemState currentState = addItemViewModel.getState();
-                currentState.setItemName(itemtitleInput.getText() + e.getKeyChar());
+                currentState.setItemName(itemTitleInput.getText() + e.getKeyChar());
                 addItemViewModel.setState(currentState);
 
             }
@@ -93,41 +100,13 @@ public class AddItemView extends JPanel implements ActionListener, PropertyChang
             }
         );
 
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(title);
+        this.add(itemTitleInfo);
+        this.add(itemDiscInfo);
+        this.add(buttons);
     }
 
-
-
-    public static void main(String[] args) {
-        frame.setTitle("Add Item");
-        frame.setSize(600,400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(panel);
-
-        panel.setLayout(null);
-        itemTitle= new JLabel("Item:");
-        itemTitle.setBounds(50, 40, 80, 25);
-        panel.add(itemTitle);
-
-        itemtitleInput = new JTextField(100);
-        itemtitleInput.setBounds(100, 40, 365, 25);
-        panel.add(itemtitleInput);
-
-        itemDisc = new JLabel("Description:");
-        itemDisc.setBounds(10, 80, 80, 25);
-        panel.add(itemDisc);
-
-        itemDiscInput = new JTextField(100);
-        itemDiscInput.setBounds(100, 80, 365,50);
-        panel.add(itemDiscInput);
-
-        saveItem = new JButton("Save");
-        saveItem.setBounds(250,150,80,25);
-        panel.add(saveItem);
-
-        frame.setVisible(true);
-
-
-    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -136,12 +115,13 @@ public class AddItemView extends JPanel implements ActionListener, PropertyChang
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        AddItemState state = (AddItemState) evt.getNewValue();
-        setFields(state);
+        if (evt.getSource() == addItemViewModel) {
+            AddItemState state = (AddItemState) evt.getNewValue();
+            setFields(state);
+        }
 
     }
-
     private void setFields(AddItemState state) {
-        itemtitleInput.setText(state.getItemName());
+        itemTitleInput.setText(state.getItemName());
     }
 }
