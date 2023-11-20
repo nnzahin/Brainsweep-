6d27314d16bd58d9cc62563dfe714999e74c1239
Huda Sahaf <hudasahaf27@gmail.com>
2023-11-20 09:58:36 -0500

    Created AddItemUseCaseFactory: issues w creating a controller, no interactor for commonItem

---
+++ src/main/java/somethingrandom/app/AddItemUseCaseFactory.java
@@ -0,0 +1,41 @@
+package somethingrandom.app;
+
+import somethingrandom.entity.CommonItemFactory;
+import somethingrandom.interfaceadapters.ViewManagerModel;
+import somethingrandom.interfaceadapters.additem.AddItemController;
+import somethingrandom.interfaceadapters.additem.AddItemPresenter;
+import somethingrandom.interfaceadapters.additem.AddItemViewModel;
+import somethingrandom.usecase.AddActionableItemInteracter;
+import somethingrandom.usecase.AddItemDataAccessInterface;
+import somethingrandom.usecase.AddItemInputBoundary;
+import somethingrandom.usecase.AddItemOutputBoundary;
+import somethingrandom.view.AddItemView;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class AddItemUseCaseFactory {
+
+    /** Prevent instantiation. */
+    private AddItemUseCaseFactory() {}
+
+    public static AddItemView create(
+        ViewManagerModel viewManagerModel, AddItemViewModel addItemViewModel) {
+
+        try {
+            AddItemController addItemController = createAddItemUseCase(viewManagerModel, addItemViewModel, userDataAccessObject);
+            return new AddItemView(addItemController, addItemViewModel);
+        } catch (IOException e){
+            JOptionPane.showMessageDialog(null, "Error.");
+
+        }
+        return null;
+    }
+
+    private static AddItemController createAddItemUseCase(ViewManagerModel viewManagerModel, AddItemViewModel addItemViewModel, AddItemDataAccessInterface addItemDataAccessObject) throws IOException{
+        AddItemOutputBoundary addItemOutputBoundary = new AddItemPresenter(addItemViewModel, viewManagerModel);
+        CommonItemFactory commonItemFactory = new CommonItemFactory();
+
+        AddItemInputBoundary addItemInteracter = new AddItemInteracter(addItemDataAccessObject, commonItemFactory);
+    }
+}
