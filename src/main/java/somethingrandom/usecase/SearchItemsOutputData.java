5fa91b1945a64a5efacd8e465f41a7be5d2d9ff9
Huda Sahaf <hudasahaf27@gmail.com>
2023-11-26 20:21:23 -0500

    SearchItemsOutputData finished

--- src/main/java/somethingrandom/usecase/SearchItemsOutputData.java
+++ src/main/java/somethingrandom/usecase/SearchItemsOutputData.java
@@ -1,4 +1,16 @@
 package somethingrandom.usecase;
 
+import somethingrandom.entity.Item;
+
 public class SearchItemsOutputData {
+
+    private final Item item;
+
+    private boolean useCaseFailed;
+
+    public SearchItemsOutputData(Item item, boolean useCaseFailed){
+        this.item = item;
+        this.useCaseFailed = useCaseFailed;
+    }
+
 }
