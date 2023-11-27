16f8af4e4fd465dd25f2f28b4109a192170eda0f
Huda Sahaf <hudasahaf27@gmail.com>
2023-11-26 20:28:37 -0500

    Made an interactor, need to understand how data access function will be implemented in order to write execute method.

--- src/main/java/somethingrandom/usecase/SearchItemsInteractor.java
+++ src/main/java/somethingrandom/usecase/SearchItemsInteractor.java
@@ -1,6 +1,21 @@
 package somethingrandom.usecase;
 
-public class SearchItemsInteractor {
+public class SearchItemsInteractor implements SearchItemsInputBoundary{
+
+    final SearchItemsDataAccessInterface searchItemsDataAccessObject;
+
+    final SearchItemsOutputBoundary searchItemsPresenter;
+
+    public SearchItemsInteractor(SearchItemsDataAccessInterface searchItemsDataAccessInterface,
+                                 SearchItemsOutputBoundary searchItemsOutputBoundary ){
+        this.searchItemsDataAccessObject = searchItemsDataAccessInterface;
+        this.searchItemsPresenter= searchItemsOutputBoundary;
+    }
+
+    @Override
+    public void execute(SearchItemsInputData searchItemsInputData) {
+        //not sure how we're implementing this yet.
+    }
 
 
 }
