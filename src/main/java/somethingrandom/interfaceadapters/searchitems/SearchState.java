5e31836986bf5669c4715fdd91595d40a0938ecd
fahimfa3 <farishtay.fahim@mail.utoronto.ca>
2023-11-30 17:01:21 -0500

    Implemented Search View. Has errors because of missing interface adapters. Once interface adapters are in I'll test this code.

--- src/main/java/somethingrandom/interfaceadapters/searchitems/SearchState.java
+++ src/main/java/somethingrandom/interfaceadapters/searchitems/SearchState.java
@@ -3,11 +3,12 @@ package somethingrandom.interfaceadapters.searchitems;
 public class SearchState {
     private String search = "";
     private String searchError = "This item does not exist.";
+
     public SearchState(){}
     public String getSearchError(){return searchError;}
-    public void setSearch(String search){this.search = search;}
+    public void setSearchQuery(String search){this.search = search;}
 
-    public String getSearch(){return this.search;}
+    public String getSearchQuery(){return this.search;}
 
 
 }
