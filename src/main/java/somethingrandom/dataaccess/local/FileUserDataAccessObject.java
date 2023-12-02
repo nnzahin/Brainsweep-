b82d988a8d03d4048900eb43a8433d00aa9721d8
Zahin Nain <zahin.nain@mail.utoronto.ca>
2023-12-01 19:40:12 -0500

    Implementing getAllItems() for search use case

--- src/main/java/somethingrandom/dataaccess/local/FileUserDataAccessObject.java
+++ src/main/java/somethingrandom/dataaccess/local/FileUserDataAccessObject.java
@@ -2,24 +2,23 @@ package somethingrandom.dataaccess.local;
 
 import org.json.JSONException;
 import org.json.JSONWriter;
-import somethingrandom.entity.ActionableItem;
-import somethingrandom.entity.DelayedItem;
-import somethingrandom.entity.Item;
-import somethingrandom.entity.ReferenceItem;
+import somethingrandom.entity.*;
 import somethingrandom.usecase.DataAccessException;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
 
 public class FileUserDataAccessObject {
     private final File dataFile;
     private final Map<UUID, Map<String, String>> items;
+    private final ItemFactory factory;
 
-    public FileUserDataAccessObject(String dataFilePath) throws DataAccessException {
+    public FileUserDataAccessObject(String dataFilePath, ItemFactory factory) throws DataAccessException {
         dataFile = new File(dataFilePath);
         items = new HashMap<>();
+        this.factory = factory;
         if (dataFile.length() == 0) {
             save();
         } else {
@@ -28,7 +27,9 @@ public class FileUserDataAccessObject {
     }
 
     private void load(){
+        try {
 
+        } catch ()
     }
 
     /**
@@ -46,7 +47,7 @@ public class FileUserDataAccessObject {
         Map<String, String> itemData = new HashMap<>();
         saveItemHelper(itemData, item);
         if (item.getNeededTime() == null){
-            itemData.put("neededTime", "null");
+            itemData.put("neededTime", "");
         } else {
             itemData.put("neededTime", item.getNeededTime().toString());
         }
@@ -84,5 +85,31 @@ public class FileUserDataAccessObject {
         } catch (IOException e) {
             throw new DataAccessException("Error while saving data");
         }
+    }
+
+    public Collection<Item> getAllItems() throws DataAccessException {
+        Collection<Item> all = new ArrayList<>();
+        for (UUID key: items.keySet()) {
+            if(items.get(key).get("itemKind").equals("ACTIONABLE")){
+                all.add(toItem(items.get(key).get("name"), UUID.fromString(items.get(key).get("id")), Instant.parse(items.get(key).get("creationDate")), Duration.parse(items.get(key).get("neededTime"))));
+            } else if (items.get(key).get("itemKind").equals("DELAYED")){
+                all.add(toItem(items.get(key).get("name"), UUID.fromString(items.get(key).get("id")), Instant.parse(items.get(key).get("creationDate")), Instant.parse(items.get(key).get("remindDate"))));
+            } else if (items.get(key).get("itemKind").equals("REFERENCE")){
+                all.add(toItem(items.get(key).get("name"), UUID.fromString(items.get(key).get("id")), Instant.parse(items.get(key).get("creationDate")), items.get(key).get("description")));
+            }
+        }
+        return all;
+    }
+
+    private ActionableItem toItem(String name, UUID id, Instant creationDate, Duration neededTime) {
+        return new ActionableItem(name, id, creationDate, neededTime);
+    }
+
+    private DelayedItem toItem(String name, UUID id, Instant creationDate, Instant remindDate) {
+        return new DelayedItem(name, id, creationDate, remindDate);
+    }
+
+    private ReferenceItem toItem(String name, UUID id, Instant creationDate, String description) {
+        return new ReferenceItem(name, id, creationDate, description);
     }
 }
