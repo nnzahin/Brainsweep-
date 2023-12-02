b8f8864774f30bb20159ec258fac2b0fe8609e81
fahimfa3 <farishtay.fahim@mail.utoronto.ca>
2023-12-02 13:51:51 -0500

    SearchState.

--- src/main/java/somethingrandom/interfaceadapters/searchitems/SearchState.java
+++ src/main/java/somethingrandom/interfaceadapters/searchitems/SearchState.java
@@ -1,14 +1,14 @@
 package somethingrandom.interfaceadapters.searchitems;
 
 public class SearchState {
-    private String search = "";
+    private String searchQuery = "";
     private String searchError = "This item does not exist.";
 
     public SearchState(){}
     public String getSearchError(){return searchError;}
-    public void setSearchQuery(String search){this.search = search;}
+    public void setSearchQuery(String search){this.searchQuery = search;}
 
-    public String getSearchQuery(){return this.search;}
+    public String getSearchQuery(){return this.searchQuery;}
 
 
 }
