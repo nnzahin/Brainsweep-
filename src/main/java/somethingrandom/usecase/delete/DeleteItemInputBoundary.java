8b18656b479e42101e5c742b1f47a92b13e56702
Zahin Nain <zahin.nain@mail.utoronto.ca>
2023-12-02 22:06:32 -0500

    Implementing input boundary

---
+++ src/main/java/somethingrandom/usecase/delete/DeleteItemInputBoundary.java
@@ -0,0 +1,12 @@
+package somethingrandom.usecase.delete;
+
+import java.util.UUID;
+
+public interface DeleteItemInputBoundary {
+    /**
+     * Delete an item from the database.
+     *
+     * @param id The ID of the item to delete.
+     */
+    void execute(UUID id);
+}
