3d28347a9ed7db4dbe8cad2f68571294a116c22a
fahimfa3 <farishtay.fahim@mail.utoronto.ca>
2023-12-03 00:50:08 -0500

    (Ignore) Cleaned up SearchView and added a comment to remind myself to implement the default search view. This commit is mainly just to ensure I have everything committed before I rebase.

--- src/main/java/somethingrandom/app/Brainsweep.java
+++ src/main/java/somethingrandom/app/Brainsweep.java
@@ -2,14 +2,19 @@ package somethingrandom.app;
 
 import okhttp3.OkHttpClient;
 import somethingrandom.dataaccess.google.GoogleDataAccessObject;
+import somethingrandom.dataaccess.google.auth.AuthenticationException;
 import somethingrandom.dataaccess.google.auth.LoginFlow;
 import somethingrandom.dataaccess.google.auth.S256CodeVerifier;
 import somethingrandom.interfaceadapters.ViewManagerModel;
 import somethingrandom.interfaceadapters.additem.AddItemViewModel;
+import somethingrandom.interfaceadapters.searchitems.SearchViewModel;
 import somethingrandom.usecase.AddItemDataAccessInterface;
 import somethingrandom.usecase.DataAccessException;
+import somethingrandom.usecase.search.SearchItemsDataAccessInterface;
 import somethingrandom.view.AddItemView;
+import somethingrandom.view.SearchView;
 import somethingrandom.view.ViewManager;
+import somethingrandom.app.SearchUseCaseFactory;
 
 import javax.swing.*;
 import java.awt.*;
@@ -17,15 +22,25 @@ import java.security.SecureRandom;
 
 public class Brainsweep {
     public static void main(String[] args) {
+        /*
+        Login Info
+         */
+
+        OkHttpClient client = new OkHttpClient();
+        LoginFlow loginFlow = new LoginFlow(client, System.getenv("OAUTH_CLIENT_SECRET"), new S256CodeVerifier(new SecureRandom()), GoogleDataAccessObject.getScopes());
 
         /*
         Constructing the main program view
          */
-        JFrame mainWindow = new JFrame("Brainsweep");
+        JFrame brainSweep = new JFrame("Brainsweep");
         GridLayout grid = new GridLayout();
-        mainWindow.setLayout(grid);
-        mainWindow.setSize(800, 600);
-        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        brainSweep.setLayout(grid);
+        brainSweep.setSize(800, 600);
+        brainSweep.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel left = new JPanel(grid);
+        JList<Object> taskList = new JList<>();
+
 
 
         JButton addButton = new JButton("Add+");
@@ -40,8 +55,7 @@ public class Brainsweep {
         ViewManagerModel viewManagerModel = new ViewManagerModel();
         new ViewManager(cardLayout,views, viewManagerModel);
 
-        OkHttpClient client = new OkHttpClient();
-        LoginFlow loginFlow = new LoginFlow(client, System.getenv("OAUTH_CLIENT_SECRET"), new S256CodeVerifier(new SecureRandom()), GoogleDataAccessObject.getScopes());
+
         AddItemDataAccessInterface addItemDataAccessObject;
         try {
             addItemDataAccessObject = new GoogleDataAccessObject(client, loginFlow.execute(), "Brainsweep");
@@ -60,6 +74,27 @@ public class Brainsweep {
 
         application.pack();
         application.setVisible(true);
+
+
+
+        /*
+        Search
+         */
+        SearchItemsDataAccessInterface searchItemsDataAccessObject;
+        try {
+            searchItemsDataAccessObject = new GoogleDataAccessObject(client, loginFlow.execute(), "Brainsweep");
+        } catch (DataAccessException e){
+            throw new RuntimeException(e);
+        }
+        SearchViewModel searchViewModel = new SearchViewModel();
+        SearchView searchView = SearchUseCaseFactory.create(viewManagerModel, searchViewModel, searchItemsDataAccessObject);
+        left.add(searchView, searchView.viewName);
+
+        left.setVisible(true);
+        // need to set the default view of the left panel to the list of all tasks...
+        // Not sure exactly where to put that as of now
+
+
 
 
 
