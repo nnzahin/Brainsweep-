c1cdd26490733c6d422a8e7a86bd73fd20e96aeb
Zahin Nain <zahin.nain@mail.utoronto.ca>
2023-12-02 22:02:31 -0500

    Implementing dao for delete item

---
+++ src/main/java/somethingrandom/usecase/delete/DeleteItemDataAccessInterface.java
@@ -0,0 +1,15 @@
+package somethingrandom.usecase.delete;
+
+import somethingrandom.usecase.DataAccessException;
+
+import java.util.UUID;
+
+public interface DeleteItemDataAccessInterface {
+    /**
+     * Delete an item from the database.
+     *
+     * @param id The ID of the item to delete.
+     * @throws DataAccessException if an error occurs
+     */
+    void delete(UUID id) throws DataAccessException;
+}
