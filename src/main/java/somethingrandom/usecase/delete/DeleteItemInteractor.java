80fbb8eee7cc26980ccd3b47eb8c5bafcf622560
Zahin Nain <zahin.nain@mail.utoronto.ca>
2023-12-04 01:27:07 -0500

    Interactor calls the presenter

--- src/main/java/somethingrandom/usecase/delete/DeleteItemInteractor.java
+++ src/main/java/somethingrandom/usecase/delete/DeleteItemInteractor.java
@@ -1,17 +1,28 @@
 package somethingrandom.usecase.delete;
 
+import somethingrandom.interfaceadapters.delete.DeleteItemPresenter;
 import somethingrandom.usecase.DataAccessException;
 
 import java.util.UUID;
 
 public class DeleteItemInteractor implements DeleteItemInputBoundary{
     private final DeleteItemDataAccessInterface dao;
+    private final DeleteItemPresenter presenter;
 
-    public DeleteItemInteractor(DeleteItemDataAccessInterface dao) {
+    public DeleteItemInteractor(DeleteItemDataAccessInterface dao, DeleteItemPresenter presenter) {
         this.dao = dao;
+        this.presenter = presenter;
     }
 
-    public void execute(UUID id) throws DataAccessException {
-        dao.delete(id);
+    public void execute(UUID id) {
+        try {
+            if(dao.delete(id)) {
+                presenter.prepareSuccessView();
+            } else {
+                presenter.prepareFailView();
+            }
+        } catch (DataAccessException e) {
+            presenter.prepareFailView();
+        }
     }
 }
